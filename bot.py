4a55427de75782523d86cdbc615aca5e52080cd4
Maggie Rosae <32102336+yvtea@users.noreply.github.com>
2017-09-24 15:39:05 -0230

    able to change the game from the cfg

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-#import discord.py libraries, secret token config
+#import discord.py libraries, config
 import discord
 import config
 from discord.ext import commands
@@ -6,7 +6,8 @@ client = discord.Client()
 # when the bot is ready, print a message to the console
 @client.event
 async def on_ready():
-    print('{0.user} online!'.format(client))
+    print('{0.user} is online!'.format(client))
+    await client.change_presence(game=discord.Game(name=config.game))
 # when a message is sent, check to see if the author is the bot. if not--
 @client.event
 async def on_message(message):
