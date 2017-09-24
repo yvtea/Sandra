5d9c3ab97de6ee951c0a58e27ace0a2141a1e182
Maggie Rosae <32102336+yvtea@users.noreply.github.com>
2017-09-24 21:07:54 -0230

    added a way to roll a d20

--- bot.py
+++ bot.py
@@ -1,32 +1,40 @@
-#import discord.py libraries, config
+#load discord.py libraries, config
 
 import discord
+from discord.ext import commands
 import config
 import random
 from datetime import datetime
-from discord.ext import commands
+
 client = discord.Client()
 now = datetime.now()
+
 # when the bot is ready, print a message to the console
 
 @client.event
-async def on_ready():
+async def on_ready():  
+
     print('{0.user} is now online!'.format(client))
     await client.change_presence(game=discord.Game(name=config.game))
-
-# when a message is sent, check to see if the author is the bot. if not--
+# when a message is sent, check to see if the message author is the bot. if not--
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-#check to see if the message starts with the prefix + a command, if so, send a message
+#check to see if the message starts with the prefix + a command, if so, execute the command
 
     if message.content.startswith(config.prefix + 'hello'):
         await message.channel.send('Hello!')
 
     elif message.content.startswith(config.prefix + 'up'):
     	await message.channel.send('I have been up since %s-%s-%s, %s:%s!' % (now.day, now.month, now.year, now.hour, now.minute))
+
+    elif message.content.startswith(config.prefix + 'code'):
+    	await message.channel.send('I was made by <@202137748885340160>, and my source code is at https://github.com/yvtea/Sandra !')
+
+    elif message.content.startswith(config.prefix + 'd20'):
+    	await message.channel.send('I rolled a %s!' % (random.randint(0, 20)))
 
 client.run(config.token)
